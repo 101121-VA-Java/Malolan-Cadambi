370033b33363729255e0078ccc02d6d679093b37
crm4803 <crm4803@gmail.com>
2021-10-13 17:56:26 -0400

    Calculator Exercise

---
+++ Calculator/Calculator.java
@@ -0,0 +1,27 @@
+package Calculator;
+
+public class Calculator{
+
+    public int add(int a, int b){
+        int c = a + b;
+        return c;
+    }
+
+    public int subtract(int a, int b){
+        int c = a - b;
+        return c;
+    }
+    //multipy
+
+    public double multiply(double a, double b){
+        double c = a * b;
+        return c;
+    }
+
+    //Reminder
+
+    public int remainder(int a, int b){
+        int c = a / b;
+        return c;
+    }
+}
---
+++ src/Calulation.java
@@ -0,0 +1,34 @@
+package src;
+
+import Calculator.Calculator;
+
+public class Calulation {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c;
+
+        Calculator calc = new Calculator();
+    
+        c = calc.add(a,b);
+    
+        System.out.println("Add Value : "+ c);
+        
+        c = calc.subtract(a,b);
+    
+        System.out.println("Subtract Value : "+ c);
+
+        double d1 = 10.5;
+        double d2 = 5;
+        double d = calc.multiply(d1, d2);
+
+        System.out.println("Multiply Vaule : "+ d);
+
+        c = calc.remainder(a, b);
+
+        System.out.println("Remainder Value : "+c);
+        
+    }
+    
+}
