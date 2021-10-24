3f64c707bb3281536f905293aa6638549762b2f0
crm4803 <crm4803@gmail.com>
2021-10-24 14:30:00 -0400

    Adding Friday Exercist Text

--- Week1/model_basics/driver/Driver.java
+++ Week1/model_basics/driver/Driver.java
@@ -13,6 +13,7 @@ public class Driver{
         Penguin three = new Penguin("Three", 14);
 
         Penguin four = new Penguin("Four", 13, "Emperor");
+        
 
         System.out.println("This is penguin " + four.name);
         System.out.println("Penguin " + three.name + " is " + three.age + " years old");
