9f76d41a9d7f8273f397c2b51120f0e227a140ae
crm4803 <crm4803@gmail.com>
2021-10-28 00:56:32 -0400

    adding project 0

---
+++ Week3/src/main/java/com/revature/models/Spices.java
@@ -0,0 +1,48 @@
+package com.revature.models;
+
+public class Spices {
+	
+	int qty;
+	
+	String name;
+	
+	String countryOfOrigin;
+
+	public void setName (String name) {
+		
+		this.name = name;
+	}
+	
+	public String getName () {
+		
+		return this.name;
+		
+	}
+	
+	public void setQty (int qty) {
+		
+		this.qty = qty;
+		
+	}
+	
+	public int getQty () {
+		
+		return this.qty;
+		
+	}
+	
+	public void setCountryOfOrigin (String countryOfOrigin) {
+		
+		this.countryOfOrigin = countryOfOrigin;
+		
+	}
+	
+	public String getCountryOfOrigin () {
+		
+		return this.countryOfOrigin;
+		
+	}
+	
+	
+	
+}
